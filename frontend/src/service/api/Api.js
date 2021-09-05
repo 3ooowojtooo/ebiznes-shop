acb4f1a65cf023c15575faa62807c9a6dafdb851
3ooowojtooo <goralczyk4@gmail.com>
2021-09-05 12:15:45 +0200

    Refactor API

--- frontend/src/service/api/Api.js
+++ frontend/src/service/api/Api.js
@@ -2,14 +2,33 @@ import axios from "axios";
 import {BACKEND_BASE_URL} from "../../const";
 import Cookies from 'js-cookie';
 
+const CSRF_TOKEN_COOKIE_NAME = "csrfToken"
+
 axios.defaults.withCredentials = true
 
 export const signOut = () => {
-    const url = BACKEND_BASE_URL + "/signOut"
-    return axios.post(url, {}, {
-        headers : {
-            "Csrf-Token": Cookies.get("csrfToken")
-        }
+    return post("/signOut")
+}
+
+function post(url, data = {}, headers = {}) {
+    const fullUrl = buildUrl(url)
+    const allHeaders = buildHeadersWithCsrfToken(headers)
+    const config = {
+        headers: allHeaders
+    }
+    return axios.post(fullUrl, data, config)
+}
+
+function buildUrl(url) {
+    return BACKEND_BASE_URL + url
+}
+
+function buildHeadersWithCsrfToken(headers) {
+    return Object.assign({}, headers, {
+        "Csrf-Token": getCsrfToken()
     })
-       // .catch(_ => window.location.reload(false))
+}
+
+function getCsrfToken() {
+    return Cookies.get(CSRF_TOKEN_COOKIE_NAME)
 }
